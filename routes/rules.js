fd5f8076dde8e13a3f31579ded99a19319408dc1
Justin McClellan <khalathas@gmail.com>
2026-06-09 17:00:09 -0400

    wired skill routes for all and by ID

--- routes/rules.js
+++ routes/rules.js
@@ -1,48 +1,55 @@
-// Main routes file for SRD specific endpoints
+// Main routes file for rules specific endpoints
 
 const express = require('express');
-const srd = express.Router();
+const rules = express.Router();
 const spellController = require('../controllers/spellController.js');
 const classController = require('../controllers/classController.js');
 const featController = require('../controllers/featController.js');
+const skillController = require('../controllers/skillController.js');
 
 // Spell routes
-srd.route('/spells')
+rules.route('/spells')
     .get(spellController.getAll);
 
-srd.route('/spells/id/:id')
+rules.route('/spells/:id')
     .get(spellController.getById);
 
-srd.route('/spells/field/:field/:value')
+rules.route('/spells/field/:field/:value')
     .get(spellController.getByField);
 
-// srd.route('/spells/class/:className') // not ready yet
+// rules.route('/spells/class/:className') // not ready yet
 
-// srd.route('/spells/class/:className/level/:level') // not ready yet
+// rules.route('/spells/class/:className/level/:level') // not ready yet
 
 // Class routes
-srd.route('/classes')
+rules.route('/classes')
     .get(classController.getAll);
 
-srd.route('/classes/book/:bookId')
+rules.route('/classes/book/:bookId')
     .get(classController.getByBookId);
 
 // Feat routes
-srd.route('/feats')
+rules.route('/feats')
     .get(featController.getAll)
     .post(featController.create);
 
-srd.route('/feats/type/:type')
+rules.route('/feats/type/:type')
     .get(featController.getByType);
 
-srd.route('/feats/:id')
+rules.route('/feats/:id')
     .get(featController.getById)
     .patch(featController.updateById)
     .delete(featController.deleteById);
 
+// Skill routes
+rules.route('/skills')
+    .get(skillController.getAll);
+
+rules.route('/skills/:id')
+    .get(skillController.getById);
 
 
 // Book routes
-// srd.route('/sourcebooks') // not ready yet
+// rules.route('/sourcebooks') // not ready yet
 
-module.exports = srd
+module.exports = rules
